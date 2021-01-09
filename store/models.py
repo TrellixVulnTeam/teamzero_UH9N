98a60b9b73dceef016a9dca2400984b2f33b33b4
Furkan <furkanceylan1999@gmail.com>
2021-01-09 05:28:30 +0300

    sth

--- store/models.py
+++ store/models.py
@@ -1,3 +1,8 @@
-from django.db import models,connection
+from django.db import models
 
+class games(models.Model):
+    name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
