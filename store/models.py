68a33b92dd3d897ae38e425701418f7ba3f7e299
Furkan <furkanceylan1999@gmail.com>
2021-01-11 17:13:23 +0300

    html

--- store/models.py
+++ store/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
-
+class Games(models.Model):
+    rank = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    platform = models.CharField(max_length=60)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=50)
+    NA_Sales = models.IntegerField()
+    EU_Sales = models.IntegerField()
+    JP_Sales = models.IntegerField()
+    Other_Sales = models.IntegerField()
+    Global_Sales = models.IntegerField()
 
