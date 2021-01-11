5788387620b485404c1eef8e02813d28b6c9104b
Furkan <furkanceylan1999@gmail.com>
2021-01-11 05:42:19 +0300

    html

--- ZeroApp/urls.py
+++ ZeroApp/urls.py
@@ -1,8 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from store.views import homepage
+from store.views import homepage,account,about_us,all_games,contact
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('homepage/',homepage)
+    path('contact/',contact),
+    path('about_us/',about_us),
+    path('account/',account),
+    path('all_games/',all_games),
+    path('homepage/',homepage),
 ]
--- store/views.py
+++ store/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 def homepage(request):
     return render(request,"homepage.html")
+def account(request):
+    return render(request,"account.html")
+def about_us(request):
+    return render(request,"about_us.html")
+def contact(request):
+    return render(request,"contact.html")
+def all_games(request):
+    return render(request,"all_games.html")
